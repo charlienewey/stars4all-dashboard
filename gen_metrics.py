05c13bf6fc30893cd90aa6b2320e79ae0d9912ba
Charles Newey <charlie@assemblyco.de>
2017-06-12 17:18:16 +0100

    Add gen_metrics.py command-line tool for dataset generation

---
+++ gen_metrics.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python3
+
+import argparse
+import sys
+
+import zoometrics
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Process some citizen science project metrics.")
+    parser.add_argument(dest="path", type=str, help="path to a CSV file with Zooniverse metrics")
+    parser.add_argument(dest="frequency", default="month", type=str,
+                        help="a frequency to group by. one of: year, month, week, day")
+    parser.add_argument(dest="dest", type=str, help="destination CSV file containing metrics")
+    args = parser.parse_args()
+
+    dataset = zoometrics.Dataset(path=args.path)
+    metrics = dataset.get_metrics(frequency=args.frequency)
+
+    metrics.to_csv(args.dest)
